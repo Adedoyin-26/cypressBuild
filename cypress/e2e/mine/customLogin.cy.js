39059ab7dfd0a5b0c6ee370f432a1ec64678e7f8
Adedoyin-26 <ibraheemadedoyin26@gmail.com>
2025-10-19 11:55:13 +0100

    POM

--- cypress/e2e/mine/customLogin.cy.js
+++ cypress/e2e/mine/customLogin.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import Login from "../../pageObjects/Login";
+
 describe('Custom command to login', () => {
     it('should use custom command', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
@@ -8,4 +10,19 @@ describe('Custom command to login', () => {
         cy.get('.oxd-text.oxd-text--p').should('exist', 'Time at Work');
     });
 });
+    it('Login test', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        
+        cy.wait(5000);
+
+        const lgn = new Login();
+        
+        cy.fixture('Orangehrm').then((data) => {
+            lgn.enterUsername(data.username);
+            lgn.enterPassword(data.password);
+            lgn.clickLogin();
+            lgn.verifyLogin(data.expectedTitle);
+    });
+    });
+
 });
